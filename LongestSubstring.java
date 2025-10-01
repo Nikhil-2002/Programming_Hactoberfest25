ad23bc2d216df66a0f2b0850d909dd0ef4ede672
Aditya Kolhe <adikolhe02@gmail.com>
2025-10-01 18:34:48 +0530

    added leetcode problem

---
+++ LongestSubstring.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class LongestSubstring {
+    public static int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int left = 0, maxLen = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            
+            // If duplicate found, shrink window
+            while (set.contains(c)) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(c);
+            maxLen = Math.max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println("Input: " + s);
+        System.out.println("Longest Substring Without Repeating Characters: " 
+                            + lengthOfLongestSubstring(s));
+    }
+}
