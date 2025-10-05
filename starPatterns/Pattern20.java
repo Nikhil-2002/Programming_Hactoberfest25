50eece80500298c347d145de523bbd026a1c2740
Aditya chandel <adityachandel371@gmail.com>
2025-10-05 19:34:11 +0530

    Add my contribution + changed folder structure

---
+++ starPatterns/Pattern20.java
@@ -0,0 +1,11 @@
+package starPatterns;
+
+import java.util.Scanner;
+
+public class Pattern20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();// 7
+
+    }
+}
---
+++ starPatterns/Pattern8.java
@@ -0,0 +1,10 @@
+package starPatterns;
+
+import java.util.Scanner;
+
+public class Pattern8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
+}
